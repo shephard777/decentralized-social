b7f374726e050fc0577d09a546498b2284ddd577
Fernando Antonio Shephard <33160614+shephard777@users.noreply.github.com>
2023-04-26 05:53:27 -0400

    Importar las diferentes dependecias para comenzar con el proyecto.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
+import {
+  urlClient,
+  LENS_HUB_CONTRACT_ADDRESS,
+  queryRecommendedProfiles,
+  queryExplorePublications,
+} from "./queres";
+import LENSHUB from "./lenshub.json";
+import {ethers} from "ethers";
+import {Box, Button, Image} from "@chakra-ui/react";
+
 
 function App() {
   return (
